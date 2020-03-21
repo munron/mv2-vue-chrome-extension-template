acd33bb7a202079b0d99fdaed9c1dd053d13f95f
hideaki murotani <munroyello@gmail.com>
2020-03-21 22:29:25 +0900

    fix indent size

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        indent: ['error', 4],
+        'no-console': 0,
+        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
 };
